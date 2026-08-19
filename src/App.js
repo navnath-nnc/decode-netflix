031281b65d57cf692cfa962ee3e666ea2b78c9f3
Navnath Chavare <navnath.nnc@gmail.com>
2026-08-19 15:43:45 +0530

    created decode-netflix project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import "./output.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" p-2 m-2 text-3xl text-blue-700 bg-yellow-300">
+      Hello React
     </div>
   );
 }
